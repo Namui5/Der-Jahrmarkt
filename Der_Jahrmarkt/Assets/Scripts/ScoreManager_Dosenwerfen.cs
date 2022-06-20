a8100fb10cd511e12ec21c3f063e0961a9296938
Namui5 <104370517+Namui5@users.noreply.github.com>
2022-06-20 23:45:37 +0200

    Dosenwerfen-Punktesystem eingebaut, unnötige Szenen gelöscht, Ordner aufgeräumt

--- Der_Jahrmarkt/Assets/Scripts/ScoreManager_Dosenwerfen.cs
+++ Der_Jahrmarkt/Assets/Scripts/ScoreManager_Dosenwerfen.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager_Dosenwerfen : MonoBehaviour
 {
 
     public Text scoreText;
@@ -13,14 +14,9 @@ public class ScoreManager : MonoBehaviour
     public GameObject dose4;
     public GameObject dose5;
     public GameObject dose6;
-    public GameObject[] dosen;
-
-    public bool dose1_gezaehlt = false;
-    public bool dose2_gezaehlt = false;
-    public bool dose3_gezaehlt = false;
-    public bool dose4_gezaehlt = false;
-    public bool dose5_gezaehlt = false;
-    public bool dose6_gezaehlt = false;
+	
+    private GameObject[] dosen = new GameObject[6];
+    private bool[] gezaehlt = new bool[6];
 
     int score = 0;
     int highScore = 6;
@@ -29,17 +25,35 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         scoreText.text = "Score: " + score.ToString();
-        dose1.transform.Rotate(2, 2, 2);
+		dosen[0] = dose1;
+        dosen[1] = dose2;
+        dosen[2] = dose3;
+        dosen[3] = dose4;
+        dosen[4] = dose5;
+        dosen[5] = dose6;
+
+        // dose1.transform.Rotate(2, 2, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(score <= highScore)
-        {
-            AddPoint();
-        }
-
+		// Schleife für jede Dose
+        for(int i = 0; i < 6; i++)
+		{
+			if(gezaehlt[i] == false)
+			{
+				// vergleicht den Winkel zwischen dem "Oben" der Umgebung mit dem "Oben" von einer Dose -> wenn er größer ist als 70, wird die Dose gezählt
+				if(Vector3.Angle(Vector3.up, dosen[i].transform.up) > 70)
+				{
+					gezaehlt[i] = true;
+				}
+			}
+		}
+		
+		// zählt den Score nach oben (50 Punkte pro Dose) und gibt es auf der Tafel aus
+		score = gezaehlt.Count(c => c) * 50;
+		scoreText.text = "Score: " + score.ToString();
     }
 
     public void AddPoint()
@@ -52,7 +66,7 @@ public class ScoreManager : MonoBehaviour
                  score++;
                  scoreText.text = "Score: " + score.ToString();
              }
-         } */
+         } 
         if (dose1.transform.rotation.x != 0 && dose1.transform.rotation.y != 0 && score < highScore && dose1_gezaehlt == false)
         {
             score++;
@@ -93,6 +107,6 @@ public class ScoreManager : MonoBehaviour
             score++;
             scoreText.text = "Score: " + score.ToString();
             dose6_gezaehlt = true;
-        } 
+        } */
     }
 }
