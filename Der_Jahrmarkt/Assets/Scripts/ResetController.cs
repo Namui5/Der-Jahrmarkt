1009002a4caade6d6bddcf5fa1f6eb509fe6fa22
sonjagaess <56551384+sonjagaess@users.noreply.github.com>
2022-06-20 16:53:34 +0200

    hallo
    startscreen, dosenwerfen funktioniert

---
+++ Der_Jahrmarkt/Assets/Scripts/ResetController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetController : MonoBehaviour
+
+
+{
+    public GameObject ball;
+    void ResetButton()
+    {
+
+    }
+}
